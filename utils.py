c9a3c3d5de586fa1c78cbc0e5d28207485e8c19c
Zicong Ma <mazicong@gmail.com>
2020-09-28 09:39:41 +0100

    version

---
+++ utils.py
@@ -0,0 +1,233 @@
+import os
+import string
+import operator
+from sklearn.metrics.pairwise import pairwise_distances
+from scipy.spatial.distance import cosine as cos_dist
+
+
+def remove_punctuation(s):
+  return s.translate(str.maketrans('','',string.punctuation))
+
+def get_wiki():
+  V = 2000
+  files = glob('../large_files/enwiki*.txt')
+  all_word_counts = {}
+  for f in files:
+    for line in open(f, encoding="utf8"):
+      if line and line[0] not in '[*-|=\{\}':
+        s = remove_punctuation(line).lower().split()
+        if len(s) > 1:
+          for word in s:
+            if word not in all_word_counts:
+              all_word_counts[word] = 0
+            all_word_counts[word] += 1
+  print("finished counting")
+
+  V = min(V, len(all_word_counts))
+  all_word_counts = sorted(all_word_counts.items(), key=lambda x: x[1], reverse=True)
+
+  top_words = [w for w, count in all_word_counts[:V-1]] + ['<UNK>']
+  word2idx = {w:i for i, w in enumerate(top_words)}
+  unk = word2idx['<UNK>']
+
+  sents = []
+  for f in files:
+    for line in open(f, encoding="utf8"):
+      if line and line[0] not in '[*-|=\{\}':
+        s = remove_punctuation(line).lower().split()
+        if len(s) > 1:
+          # if a word is not nearby another word, there won't be any context!
+          # and hence nothing to train!
+          sent = [word2idx[w] if w in word2idx else unk for w in s]
+          sents.append(sent)
+  return sents, word2idx
+              
+			  
+def my_tokenizer(s):
+    s = remove_punctuation(s)
+    s = s.lower() # downcase
+    return s.split()
+  
+def get_wikipedia_data(n_files, n_vocab, by_paragraph=False):	
+    prefix = '../large_files/'
+
+    if not os.path.exists(prefix):
+        print("Are you sure you've downloaded, converted, and placed the Wikipedia data into the proper folder?")
+        print("I'm looking for a folder called large_files, adjacent to the class folder, but it does not exist.")
+        print("Please download the data from https://dumps.wikimedia.org/")
+        print("Quitting...")
+        exit()
+
+    input_files = [f for f in os.listdir(prefix) if f.startswith('enwiki') and f.endswith('txt')]
+
+    if len(input_files) == 0:
+        print("Looks like you don't have any data files, or they're in the wrong location.")
+        print("Please download the data from https://dumps.wikimedia.org/")
+        print("Quitting...")
+        exit()
+
+    # return variables
+    sentences = []
+    word2idx = {'START': 0, 'END': 1}
+    idx2word = ['START', 'END']
+    current_idx = 2
+    word_idx_count = {0: float('inf'), 1: float('inf')}
+
+    if n_files is not None:
+        input_files = input_files[:n_files]
+
+    for f in input_files:
+        print("reading:", f)
+        for line in open(prefix + f, encoding="utf8"):
+            line = line.strip()
+            # don't count headers, structured data, lists, etc...
+            if line and line[0] not in ('[', '*', '-', '|', '=', '{', '}'):
+                if by_paragraph:
+                    sentence_lines = [line]
+                else:
+                    sentence_lines = line.split('. ')
+                for sentence in sentence_lines:
+                    tokens = my_tokenizer(sentence)
+                    for t in tokens:
+                        if t not in word2idx:
+                            word2idx[t] = current_idx
+                            idx2word.append(t)
+                            current_idx += 1
+                        idx = word2idx[t]
+                        word_idx_count[idx] = word_idx_count.get(idx, 0) + 1
+                    sentence_by_idx = [word2idx[t] for t in tokens]
+                    sentences.append(sentence_by_idx)
+
+    # restrict vocab size
+    sorted_word_idx_count = sorted(word_idx_count.items(), key=operator.itemgetter(1), reverse=True)
+    word2idx_small = {}
+    new_idx = 0
+    idx_new_idx_map = {}
+    for idx, count in sorted_word_idx_count[:n_vocab]:
+        word = idx2word[idx]
+        print(word, count)
+        word2idx_small[word] = new_idx
+        idx_new_idx_map[idx] = new_idx
+        new_idx += 1
+    # let 'unknown' be the last token
+    word2idx_small['UNKNOWN'] = new_idx 
+    unknown = new_idx
+
+    assert('START' in word2idx_small)
+    assert('END' in word2idx_small)
+    assert('king' in word2idx_small)
+    assert('queen' in word2idx_small)
+    assert('man' in word2idx_small)
+    assert('woman' in word2idx_small)
+
+    # map old idx to new idx
+    sentences_small = []
+    for sentence in sentences:
+        if len(sentence) > 1:
+            new_sentence = [idx_new_idx_map[idx] if idx in idx_new_idx_map else unknown for idx in sentence]
+            sentences_small.append(new_sentence)
+
+    return sentences_small, word2idx_small
+
+def get_wikipedia_data1(n_files, n_vocab, by_paragraph=False):
+    prefix = '../large_files/'
+
+    if not os.path.exists(prefix):
+        print("Are you sure you've downloaded, converted, and placed the Wikipedia data into the proper folder?")
+        print("I'm looking for a folder called large_files, adjacent to the class folder, but it does not exist.")
+        print("Please download the data from https://dumps.wikimedia.org/")
+        print("Quitting...")
+        exit()
+
+    input_files = [f for f in os.listdir(prefix) if f.startswith('enwiki') and f.endswith('txt')]
+
+    if len(input_files) == 0:
+        print("Looks like you don't have any data files, or they're in the wrong location.")
+        print("Please download the data from https://dumps.wikimedia.org/")
+        print("Quitting...")
+        exit()
+
+    # return variables
+    sentences = []
+    word2idx = {}
+    idx2word = []
+    current_idx = 0
+    word_idx_count = {}
+
+    if n_files is not None:
+        input_files = input_files[:n_files]
+
+    for f in input_files:
+        print("reading:", f)
+        for line in open(prefix + f, encoding="utf8"):
+            line = line.strip()
+            # don't count headers, structured data, lists, etc...
+            if line and line[0] not in ('[', '*', '-', '|', '=', '{', '}'):
+                if by_paragraph:
+                    sentence_lines = [line]
+                else:
+                    sentence_lines = line.split('. ')
+                for sentence in sentence_lines:
+                    tokens = my_tokenizer(sentence)
+                    for t in tokens:
+                        if t not in word2idx:
+                            word2idx[t] = current_idx
+                            idx2word.append(t)
+                            current_idx += 1
+                        idx = word2idx[t]
+                        word_idx_count[idx] = word_idx_count.get(idx, 0) + 1
+                    sentence_by_idx = [word2idx[t] for t in tokens]
+                    sentences.append(sentence_by_idx)
+
+    # restrict vocab size
+    sorted_word_idx_count = sorted(word_idx_count.items(), key=operator.itemgetter(1), reverse=True)
+    word2idx_small = {}
+    new_idx = 0
+    idx_new_idx_map = {}
+    for idx, count in sorted_word_idx_count[:n_vocab]:
+        word = idx2word[idx]
+        word2idx_small[word] = new_idx
+        idx_new_idx_map[idx] = new_idx
+        new_idx += 1
+    # let 'unknown' be the last token
+    word2idx_small['UNKNOWN'] = new_idx
+    unknown = new_idx
+
+    assert('king' in word2idx_small)
+    assert('queen' in word2idx_small)
+    assert('man' in word2idx_small)
+    assert('woman' in word2idx_small)
+
+    # map old idx to new idx
+    sentences_small = []
+    for sentence in sentences:
+        if len(sentence) > 1:
+            new_sentence = [idx_new_idx_map[idx] if idx in idx_new_idx_map else unknown for idx in sentence]
+            sentences_small.append(new_sentence)
+
+    return sentences_small, word2idx_small
+	
+def find_analogies(w1, w2, w3, We, word2idx, idx2word):
+    V, D = We.shape
+
+    king = We[word2idx[w1]]
+    man = We[word2idx[w2]]
+    woman = We[word2idx[w3]]
+    v0 = king - man + woman
+
+    for dist in ('euclidean', 'cosine'):
+        distances = pairwise_distances(v0.reshape(1, D), We, metric=dist).reshape(V)
+        # idx = distances.argmin()
+        # best_word = idx2word[idx]
+        idx = distances.argsort()[:4]
+        best_idx = -1
+        keep_out = [word2idx[w] for w in (w1, w2, w3)]
+        for i in idx:
+            if i not in keep_out:
+                best_idx = i
+                break
+        best_word = idx2word[best_idx]
+
+
+        print("closest match by", dist, "distance:", best_word)
+        print(w1, "-", w2, "=", best_word, "-", w3)
